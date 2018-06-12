104500ff5a0c195a55738e9c6ec3f3161fb9a82b
fatemehKi <40069260+fatemehKi@users.noreply.github.com>
2018-06-12 17:28:08 -0400

    Update WProgram.h

--- WProgram.h
+++ WProgram.h
@@ -9,10 +9,10 @@
 // some libraries and sketches depend on this
 // AVR stuff, assuming Arduino.h or WProgram.h
 // automatically includes it...
-#include <avr/pgmspace.h>
-#include <avr/interrupt.h>
+//#include <avr/pgmspace.h>
+//#include <avr/interrupt.h>
 
-#include "avr_functions.h"
+//#include "avr_functions.h"
 #include "wiring.h"
 #include "HardwareSerial.h"
 
