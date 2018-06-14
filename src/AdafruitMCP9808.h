817d9ea3063f476940ea0fafdcda6ddd1ff041d6
fatemehKi <40069260+fatemehKi@users.noreply.github.com>
2018-06-14 12:58:31 -0400

    Update AdafruitMCP9808.h

--- src/AdafruitMCP9808.h
+++ src/AdafruitMCP9808.h
@@ -52,8 +52,8 @@
 //#define kLidarLitePreviousMeasuredDistanceLSB   0x15    // Previous low byte of calculated delay of signal
 
 // External Control Registers
-#define kLidarLiteHardwareVersion               0x41    // Hardware Version: revisions begin with 0x01
-#define kLidarLiteSoftwareVersion               0x4f    // Software Version: Revisions begin with 0x01
+#define kAdafruitMCP9808HardwareVersion               0x41    // Hardware Version: revisions begin with 0x01
+#define kAdafruitMCP9808SoftwareVersion               0x4f    // Software Version: Revisions begin with 0x01
 
 // Register Command
 #define kAdafruitMCP9808Measure                       0x04    // Take acquisition & correlation processing with DC correction
@@ -65,7 +65,7 @@ public:
     int kI2CFileDescriptor ;        // File Descriptor to the Lidar-Lite
     int error ;
     AdafruitMCP9808();
-    ~AdafruitMCP9808() ;
+    ~AdafruitMCP9808();
     bool openAdafruitMCP9808() ;                   // Open the I2C bus to the Lidar-Lite
     void closeAdafruitMCP9808();                   // Close the I2C bus to the Lidar-Lite
     int writeAdafruitMCP9808(int writeRegister,int writeValue) ;
